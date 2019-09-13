5d7b3c0d6018d12a4e71d2ef8ee91e34bedb80a6
Jenny Graham-Jones <jennygrahamjones@users.noreply.github.com>
2019-09-13 17:25:16 +0100

    cotd: loading sample fishies

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-
+import sampleFishes from "../sample-fishes";
+import Fish from "./Fish";
 import Header from "./Header";
 import Inventory from "./Inventory";
 import Order from "./Order";
@@ -13,13 +14,25 @@ class App extends React.Component {
     this.setState({ fishes });
   };
 
+  loadSampleFishes = () => {
+    this.setState({ fishes: sampleFishes });
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map(key => (
+              <Fish key={key} details={this.state.fishes[key]} />
+            ))}
+          </ul>
         </div>
-        <Inventory addFish={this.addFish} />
+        <Inventory
+          addFish={this.addFish}
+          loadSampleFishes={this.loadSampleFishes}
+        />
         <Order />
       </div>
     );
--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -8,6 +8,9 @@ class Inventory extends React.Component {
       <div className="inventory">
         <h2>Inventory</h2>
         <AddFishForm addFish={this.props.addFish} />
+        <button onClick={this.props.loadSampleFishes}>
+          Load sample fishes
+        </button>
       </div>
     );
   }

eff5165a617e3ba04b5ec8fa6dda913db8763ea7
Jenny Graham-Jones <jennygrahamjones@users.noreply.github.com>
2019-09-13 14:58:23 +0100

    cotd: introduce props and functional components

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 import Header from "./Header";
 import Inventory from "./Inventory";
 import Order from "./Order";
@@ -8,7 +9,7 @@ class App extends React.Component {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
-          <Header />
+          <Header tagline="Fresh Seafood Market" />
         </div>
         <Inventory />
         <Order />

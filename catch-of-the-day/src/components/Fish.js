238355fbc2a53191d30bbca81cd97b6b0c920698
Jenny Graham-Jones <jennygrahamjones@users.noreply.github.com>
2019-09-13 17:29:38 +0100

    cotd: loading sample fishies (#3)

---
+++ catch-of-the-day/src/components/Fish.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { formatPrice } from "../helpers";
+
+class Fish extends React.Component {
+  render() {
+    const { image, name, price, status, desc } = this.props.details;
+    const isAvailable = status === "available";
+
+    return (
+      <li className="menu-fish">
+        <img src={image} alt={name} />
+        <h3 className="fish-name">
+          {name}
+          <span className="price">{formatPrice(price)}</span>
+        </h3>
+        <p>{desc}</p>
+        <button disabled={!isAvailable}>Add To Cart</button>
+      </li>
+    );
+  }
+}
+
+export default Fish;

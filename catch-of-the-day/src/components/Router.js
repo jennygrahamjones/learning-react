d4052065984ab6d9b899920088f39af76d1cc85f
Jenny Graham-Jones <jennygrahamjones@users.noreply.github.com>
2019-09-13 15:36:23 +0100

    cotd: auto route based on form

--- catch-of-the-day/src/components/Router.js
+++ catch-of-the-day/src/components/Router.js
@@ -9,7 +9,7 @@ const Router = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={StorePicker} />
-      <Route path path="/store/:storeId" component={App} />
+      <Route path="/store/:storeId" component={App} />
       <Route component={NotFound} />
     </Switch>
   </BrowserRouter>
--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,15 +1,35 @@
 import React from "react";
 
+import { getFunName } from "../helpers";
+
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+
+  /* Without the arrow syntax here, it would be necessary to declare a constructor
+     and explicitly bind each function, otherwise 'this' would be undefined */
+  goToStore = event => {
+    event.preventDefault();
+    // Get the text from the input
+    const storeName = this.myInput.current.value;
+    // Change the page to /store/whatever-they-entered
+    this.props.history.push(`/store/${storeName}`);
+  };
+
   render() {
     // return React.createElement("p", { classname: "hey" }, "heyyyy"); <-- non-JSX syntax for mixing HTML with JS... yuck?
     return (
       <>
         {/* An empty tag is equivalent to using <React.Fragment> and allows returning sibling elements within render() */}
-        <form action="" className="store-selector">
+        <form className="store-selector" onSubmit={this.goToStore}>
           {/* className instead of class! */}
           <h2>Please enter a store</h2>
-          <input type="text" required placeholder="Store name" />
+          <input
+            type="text"
+            ref={this.myInput}
+            required
+            placeholder="enter store name"
+            defaultValue={getFunName()}
+          />
           <button type="submit">Visit store</button>
         </form>
       </>

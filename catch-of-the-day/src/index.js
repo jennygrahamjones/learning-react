ce46e922b26ee0c321a6d163b025adf8519e7bc9
Jenny Graham-Jones <jennygrahamjones@users.noreply.github.com>
2019-09-13 15:08:31 +0100

    cotd: add routing

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,6 +1,8 @@
-import React from "react";
-import { render } from "react-dom";
-import App from "./components/App";
 import "./css/style.css";
 
-render(<App />, document.querySelector("#main"));
+import React from "react";
+import { render } from "react-dom";
+
+import Router from "./components/Router";
+
+render(<Router />, document.querySelector("#main"));

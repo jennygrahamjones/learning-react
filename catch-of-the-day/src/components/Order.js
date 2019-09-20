eab04a07d995ab237d76680e899a051950823f35
Jenny Graham-Jones <jennygrahamjones@users.noreply.github.com>
2019-09-20 13:56:26 +0100

    cotd: refactoring

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -1,29 +1,34 @@
 import React from "react";
 import { formatPrice } from "../helpers";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 class Order extends React.Component {
   renderOrder = key => {
     const fish = this.props.fishes[key];
     // make sure fish are loaded before rendering
-    if (!fish) {
-      return null;
-    }
+    if (!fish) return null;
+
     const count = this.props.order[key];
     const isAvailable = fish.status === "available";
-    if (!isAvailable) {
-      return (
+
+    return (
+      <CSSTransition
+        classNames="order"
+        key={key}
+        timeout={{ enter: 250, exit: 250 }}
+      >
         <li key={key}>
-          Sorry {fish ? fish.name : "fish"} is no longer unavailable
+          {isAvailable ? (
+            <>
+              {count} lbs {fish.name}
+              {formatPrice(count * fish.price)}
+            </>
+          ) : (
+            <>Sorry {fish ? fish.name : "fish"} is no longer unavailable</>
+          )}
         </li>
-      );
-    } else {
-      return (
-        <li key={key}>
-          {count} lbs {fish.name}
-          {formatPrice(count * fish.price)}
-        </li>
-      );
-    }
+      </CSSTransition>
+    );
   };
 
   render() {
@@ -37,7 +42,9 @@ class Order extends React.Component {
     return (
       <div className="order-wrap">
         <h2>Order</h2>
-        <ul className="order">{orderIds.map(this.renderOrder)}</ul>
+        <TransitionGroup component="ul" className="order">
+          {orderIds.map(this.renderOrder)}
+        </TransitionGroup>
         <div className="total">
           Total:
           <strong>{formatPrice(total)}</strong>

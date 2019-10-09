72adf59f666cb52b1860d0b132607cc72c9e5953
Jenny Graham-Jones <jennygrahamjones@users.noreply.github.com>
2019-10-09 15:22:30 +0100

    rdx: introduce photo component

--- reduxstagram/client/components/PhotoGrid.js
+++ reduxstagram/client/components/PhotoGrid.js
@@ -1,8 +1,15 @@
 import React from "react";
+import Photo from "./Photo";
 
 const PhotoGrid = React.createClass({
   render() {
-    return <div className="photo-grid">I'm the photo grid</div>;
+    return (
+      <div className="photo-grid">
+        {this.props.posts.map((post, index) => (
+          <Photo {...this.props} key={index} index={index} post={post} />
+        ))}
+      </div>
+    );
   }
 });
 export default PhotoGrid;

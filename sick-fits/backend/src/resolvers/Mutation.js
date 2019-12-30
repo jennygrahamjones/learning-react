d8df8b9b8abfcd4e51bf631be981c7a0debe7fcd
Jenny Graham-Jones <jennygrahamjones@users.noreply.github.com>
2019-12-30 15:31:32 +0000

    Update data model - change filetype to graphql - add item type - add placeholder methods to schema

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,4 @@
+// methods defined in here must match those defined on the Mutation type in the schema
 const Mutations = {};
 
 module.exports = Mutations;
--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,4 @@
+// methods defined in here must match those defined on the Query type in the schema
 const Query = {};
 
 module.exports = Query;

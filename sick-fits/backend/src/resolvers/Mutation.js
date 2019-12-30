1f9ac1de9d3e69d41255a9876a73d6473b9cf3ce
Jenny Graham-Jones <jennygrahamjones@users.noreply.github.com>
2019-12-30 15:48:37 +0000

    sf: add first query and mutation

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,4 +1,11 @@
 // methods defined in here must match those defined on the Mutation type in the schema
-const Mutations = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    // check if they are logged in...
+    const item = await ctx.db.mutation.createItem({ data: { ...args } }, info);
+    // alternatively, this could be non-async and returned as a Promise
+    return item;
+  }
+};
 
 module.exports = Mutations;
--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,4 +1,12 @@
 // methods defined in here must match those defined on the Query type in the schema
-const Query = {};
+const { forwardTo } = require("prisma-binding");
+
+const Query = {
+  //   async items(parent, args, ctx, info) {
+  //     const items = await ctx.db.query.items();
+  //     return items;
+  //   }
+  items: forwardTo("db")
+};
 
 module.exports = Query;

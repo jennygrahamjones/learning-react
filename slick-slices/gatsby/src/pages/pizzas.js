68456174974a2924c0768e51b6cc31e5022033ff
jennygrahamjones <jennygrahamjones@users.noreply.github.com>
2020-09-25 17:02:56 +0100

    Layouts n that

--- slick-slices/gatsby/src/pages/pizzas.js
+++ slick-slices/gatsby/src/pages/pizzas.js
@@ -1,9 +1,5 @@
 import React from 'react';
 
 export default function PizzasPage() {
-  return (
-    <div>
-      <p>Hey, I'm the pizzas page!</p>
-    </div>
-  );
+  return <p>Hey, I'm the pizzas page!</p>;
 }
--- slick-slices/gatsby/src/pages/slicemasters.js
+++ slick-slices/gatsby/src/pages/slicemasters.js
@@ -1,9 +1,5 @@
 import React from 'react';
 
 export default function SlicemastersPage() {
-  return (
-    <div>
-      <p>Hey, I'm the slicemasters page!</p>
-    </div>
-  );
+  return <p>Hey, I'm the slicemasters page!</p>;
 }

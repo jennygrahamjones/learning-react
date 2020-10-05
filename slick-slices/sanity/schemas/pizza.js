bd8518ba628587a6e1363f30af3404f16ee9681a
Jenny Graham-Jones <jennygrahamjones@users.noreply.github.com>
2020-10-05 21:53:03 +0100

    slices: initial setup of sanity schema

---
+++ slick-slices/sanity/schemas/pizza.js
@@ -0,0 +1,45 @@
+import { FaPizzaSlice as icon } from "react-icons/fa";
+
+export default {
+  // computer name
+  name: "pizza",
+  // visible title
+  title: "Pizzas",
+  type: "document",
+  icon: icon,
+  fields: [
+    {
+      name: "name",
+      title: "Pizza name",
+      type: "string",
+      description: "The name of the pizza",
+    },
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "name",
+        maxLength: 100,
+      },
+    },
+    {
+      name: "image",
+      title: "Image",
+      type: "image",
+      options: {
+        hotspot: true, // hotspot import area, for better previews/crops!
+      },
+    },
+    {
+      name: "price",
+      title: "Price",
+      type: "number",
+      description: "Price of the pizza in cents",
+      options: {
+        validation: (Rule) => Rule.min(1000).max(5000), // can contain any logic
+        // TODO: add custom input component
+      },
+    },
+  ],
+};

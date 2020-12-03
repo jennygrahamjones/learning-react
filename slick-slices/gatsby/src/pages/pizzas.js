17254b497e804675a1bc935cb99c1049c48fc191
jennygrahamjones <jennygrahamjones@users.noreply.github.com>
2020-12-03 14:04:47 -0500

    feat: add toppings filter foundation

--- slick-slices/gatsby/src/pages/pizzas.js
+++ slick-slices/gatsby/src/pages/pizzas.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 import PizzaList from '../components/PizzaList';
+import ToppingsFilter from '../components/ToppingsFilter';
 
 export default function PizzasPage({ data }) {
   const pizzas = data.pizzas.nodes;
   return (
     <>
+      <ToppingsFilter />
       <PizzaList pizzas={pizzas} />
     </>
   );
